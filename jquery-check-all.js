f822782d710abcb429d93d1086e6bae031930d2d
Mark Guerra <markgx@gmail.com>
2015-07-19 10:13:33 -0700

    update for version 0.5.0

--- jquery-check-all.js
+++ jquery-check-all.js
@@ -1,15 +1,3 @@
-/*
-  https://github.com/markgx/jquery-check-all
-
-  Copyright (c) 2012 Mark Guerra <markgx@gmail.com>
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
 ;(function ($, window, document) {
   var pluginName = 'checkAll';
 
@@ -21,7 +9,7 @@
 
   function checkAll(element, options) {
     this.$el = $(element);
-    this.options = $.extend({}, defaults, options) ;
+    this.options = $.extend({}, defaults, this.$el.data(), options) ;
     this.init();
   }
 
@@ -59,6 +47,7 @@
       checkedCount > 0 && checkedCount < totalCount;
 
     this.$el.prop('indeterminate', indeterminate);
-    this.$el.prop('checked', checkedCount === totalCount);
+    this.$el.prop('checked', checkedCount === totalCount && totalCount !== 0);
   }
+
 })(jQuery, window, document);
